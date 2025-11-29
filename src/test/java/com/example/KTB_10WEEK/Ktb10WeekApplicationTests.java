3f8bb79fa2a6c24a800b9ef34763d6d9227e616d
yeongmin <hwongyoungmin@gmail.com>
2025-11-29 17:40:59 +0900

    ADD: Jacoco 설정, 전체 커버리지 페이지, 유저 도에민 커버리지 페이지 분리

--- src/test/java/com/example/KTB_10WEEK/Ktb10WeekApplicationTests.java
+++ src/test/java/com/example/KTB_10WEEK/Ktb10WeekApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ktb4WeekApplicationTests {
+class Ktb10WeekApplicationTests {
 
 	@Test
 	void contextLoads() {
